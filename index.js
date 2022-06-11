b68595f2b0bf2f3f2bd234439f941b3f53607c50
evolshenanigans <gjgutierrez91@gmail.com>
2022-06-11 15:33:04 -0700

    main

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Message, MessageActionRow, MessageButton } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
@@ -12,6 +12,45 @@ client.on('messageCreate', (message) => {
 
     if(message.content === 'bing bong') {
         message.reply('FUCK YO LIFE');
+    }
+})
+
+
+
+// tic tac toe code
+let EMPTY = Symbol("empty");
+
+let tictactoe_state 
+function makeGrid() {
+    components = []
+
+    messageButton = new MessageButton()
+        .setCustomId('tictactoe')
+        .setLabel('test')
+        .setStyle('SECONDARY')
+
+    actionRow = new MessageActionRow()
+    actionRow.addComponents(messageButton)
+
+    components.push(actionRow)
+    return components
+}
+
+
+client.on('interactionCreate', async interaction => {
+    if(!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'tictactoe') {
+        
+
+        let tictactoe_state = [
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]
+        ]
+        await interaction.reply({ content: 'Playing a game of tic tac toe!', components: makeGrid()});
     }
 })
 
